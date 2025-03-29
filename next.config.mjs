973890cb2ec6e26de21e234d970bc0b8f0d91aee
ayushkumar789 <22eg110b05@anurag.edu.in>
2025-03-29 18:25:09 +0530

    obfuscated.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import WebpackObfuscator from 'webpack-obfuscator';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
@@ -11,6 +13,23 @@ const nextConfig = {
                 hostname: '**.supabase.co',
             },
         ],
+    },
+    webpack(config, { dev, isServer }) {
+        // Only obfuscate client-side code in production
+        if (!dev && !isServer) {
+            config.plugins.push(
+                new WebpackObfuscator(
+                    {
+                        // You can adjust these options for more or less obfuscation.
+                        rotateStringArray: true,
+                        stringArray: true,
+                        stringArrayThreshold: 0.75,
+                    },
+                    []
+                )
+            );
+        }
+        return config;
     },
 };
 
